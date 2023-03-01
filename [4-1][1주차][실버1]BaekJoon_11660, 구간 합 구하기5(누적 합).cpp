1f6615ca40cc755830a5590e607ef21078619bc3
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2023-03-01 15:41:03 +0900

    BaekJoon_11660, 구간 합 구하기5(누적 합)

---
+++ [4-1][1주차][실버1]BaekJoon_11660, 구간 합 구하기5(누적 합).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int n, m, x_1, x_2, y_1, y_2;
+int Sum[1025][1025];
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> Sum[i][j];
+		}
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			Sum[i][j] += Sum[i][j - 1];
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			Sum[j][i] += Sum[j - 1][i];
+		}
+	}
+
+	while (m--) {
+		cin >> x_1 >> y_1 >> x_2 >> y_2;
+		cout << Sum[x_2][y_2] + Sum[x_1 - 1][y_1 - 1] - Sum[x_2][y_1-1] - Sum[x_1-1][y_2] << endl;
+	}
+
+	return 0;
+}
